fcfdde9175c0d9f9364cbf54c348374152e78e6a
Ryan Dill <ryand1127@gmail.com>
2021-03-02 18:47:50 -0500

    Finished the Login Form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,6 +2,53 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import * as yup from 'yup';
 import loginFormSchema from '../utils/loginFormSchema';
+import styled from 'styled-components'
+
+//CSS Styling
+const StyledForm = styled.div`
+text-align: center;
+
+
+h2{
+  font-size: 3.2rem;
+  border-bottom: 1px solid black;
+  width: 75%;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+p{
+  margin-top: -2%;
+  color: grey;
+  font-size: 1.2rem;
+}
+
+form{
+  background-color: #e6ebe7;
+  border-radius: 25px 25px;
+  width: 50%;
+  height: 40vh;
+  margin: 0 auto;
+}
+
+input{
+  font-size: 1.2rem;
+  margin: 0 5% 5% 1%;
+}
+
+button{
+  font-size: 1.2rem;
+  border-radius: 15px 15px;
+  padding: 8px;
+  background-color: #acc8af;
+  border: 1px solid grey;
+
+    &:hover{
+      transform: scale(1.2);
+      transition: 0.5s;
+    }
+}
+`
 
 //Creating initial login values
 const initialLogin = {
@@ -30,25 +77,29 @@ const LoginForm = () => {
 
   //Allowing target value to be changed
   const changeHandler = (e) => {
-    const {name, value, type, checked} = e.target
-    const valueToUse = type === 'checkbox' ? checked : value
-    setLoginErrors(name, valueToUse)
-    setLoginValue({...loginValue, [name]: valueToUse})
+    const {name, value} = e.target
+    setLoginErrors(name, value)
+    setLoginValue({...loginValue, [name]: value})
   };
 
   //Creating submit event
   const submitHandler = event => {
     event.preventDefault()
+    // setLoginValue({ username:'', password:''})
     // const newUser = { username: loginValue.username.trim(), password: loginValue.password.trim() }
     //Axios Post
   }
 
   return (
-    <div>
+    <StyledForm>
       <form onSubmit={submitHandler}>
-        <title> </title>
-        <h2>User login</h2>
         <div>
+          <h2>User login</h2>
+          <p>Sign in below</p>
+        </div>
+        <div>
+        <div style={{color : 'red'}}>{errors.password}</div>
+        <div style={{color : 'red'}}>{errors.username}</div>
             <label>
                 <input
                 name="username" 
@@ -56,8 +107,7 @@ const LoginForm = () => {
                 value={loginValue.username}
                 onChange={changeHandler}
                 placeholder="Username"
-                />
-                <div style={{color : 'red'}}>{errors.username}</div>
+                /> 
             </label>
             <label>
                 <input
@@ -67,15 +117,14 @@ const LoginForm = () => {
                 onChange={changeHandler}
                 placeholder="Password"
                 />
-                <div style={{color : 'red'}}>{errors.password}</div>
             </label>
                 <button disabled={disabled}>Login</button>   
         </div>
         <div>
-          <button>Sign up!</button>
+          <button>No account? Sign up!</button>
         </div>
       </form>
-    </div>
+    </StyledForm>
   );
 };
 
