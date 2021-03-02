f06cb750e715dc7bfb18a21f6be89eaf718dc84e
CarlosSantiago Nevett <csnevett@gmail.com>
2021-03-02 12:54:31 -0500

    auth

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -20,22 +20,21 @@ export const signUpLoading = (signup) => (dispatch) => {
   dispatch({ type: SIGN_UP_LOADING });
 
   axiosWithAuth()
-  .post('/auth/register', signup)
-  .then(res => {
-    console.log("SignUpSuccess", res)
-    dispatch({type:SIGN_UP_SUCCESS, payload: res.data})
-    window.location.href = '/login'
-  })
-  .catch(err => {
-    console.log('SignUpFail:' err);
-    dispatch({type: SIGN_UP_FAILURE, payload: err.message})
-  })
+    .post('/auth/register', signup)
+    .then((res) => {
+      console.log('SignUpSuccess', res);
+      dispatch({ type: SIGN_UP_SUCCESS, payload: res.data });
+      window.location.href = '/login';
+    })
+    .catch((err) => {
+      console.log('SignUpFail:', err);
+      dispatch({ type: SIGN_UP_FAILURE, payload: err.message });
+    });
 };
 
 export const loginLoading = (login) => (dispatch) => {
   dispatch({ type: LOGIN_LOADING });
   window.localStorage.removeItem('token');
-  
 };
 
 export const updateUser = (userId, update) => (dispatch) => {
