5af4a1250b4fb16b43292a79cf71cfe97c741953
CarlosSantiago Nevett <csnevett@gmail.com>
2021-03-03 10:43:00 -0500

    resolving merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import UserForm from './components/UserForm';
 import LoginForm from './components/LoginForm';
-import PlantForm from './components/PlantForm';
 import Header from '../src/components/Header';
 
 const App = () => {
