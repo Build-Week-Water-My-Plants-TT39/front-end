9a608798129554c168a57328f160d1fc70b6e049
AD9018 <adamdadi23@gmail.com>
2021-03-03 10:09:52 -0500

    finished userForm/plantForm userSchema/plantSchema

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import UserForm from './components/UserForm'
+import LoginForm from './components/LoginForm'
+import PlantForm from './components/PlantForm'
+
 
 const App = () => {
   return (
     <div className="App">
       <h1>Water our plant app!</h1>
+      <PlantForm /> 
     </div>
   );
 };
